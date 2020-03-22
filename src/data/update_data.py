4c8f3091d128b6a5a8b7873cd90b4496f54ed51b
ZhaohengLi <zhaohengli.thu@gmail.com>
2020-03-22 18:21:53 +0800

    Added some comments.

--- src/data/update_data.py
+++ src/data/update_data.py
@@ -8,8 +8,9 @@ sys.path.append('..')
 sys.path.append('../..')
 import time
 import threading
+import traceback
 from src.libs.log import L
-from src.data.dxy_record import request_data_province
+from src.data.dxy_record import *
 
     
 def worker():
@@ -18,8 +19,8 @@ def worker():
         request_rumor_data()
         request_news_data()
         L.info("Sleep now, worker will run after 30 minutes")
-    except:
-        L.info("Someting went wrong!")
+    except Exception as e:
+        L.info("Someting went wrong because {}".format(str(e)))
         
 def run():
     while True:
